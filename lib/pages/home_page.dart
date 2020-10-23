84c581598ebb1db471d9fac76470c5e015c086eb
luoweijie <luoweijie@52tt.com>
2020-10-23 17:05:52 +0800

    网络请求

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,21 +1,35 @@
 /*
  * @Author: your name
  * @Date: 2020-10-22 15:28:11
- * @LastEditTime: 2020-10-23 15:03:11
- * @LastEditors: your name
+ * @LastEditTime: 2020-10-23 17:05:20
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /my_app/lib/pages/home_page.dart
  */
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-// import 'package:dio/dio.dart';
+Dio dioHttp = new Dio();
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return Scaffold(
       body:Center(
         child:Text('商城首页')
       )
     );
+  }
+}
+
+void getHttp() async {
+  try{
+    Response response;
+    // response = await dioHttp.get("https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=小飞侠");
+    response = await dioHttp.get("https://flutterchina.club/networking/");
+   return print(response);
+    // var 
+  }catch(e){
+    return print(e);
   }
 }
